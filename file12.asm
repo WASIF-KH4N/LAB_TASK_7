bdc39f00351b40019f479954329277061ec1c232
Wasif Khan <m.wasif9905@gmail.com>
2024-09-07 10:49:35 +0500

    Add files via upload

---
+++ file12.asm
@@ -0,0 +1,56 @@
+;Program to input from the user whether the number is odd or even.
+
+Dosseg
+.model small
+.stack 100h
+.data 
+Var_1 db 'NUMBER IS ODD$'
+Var_2 db 'NUMBER IS EVEN$'
+
+.code
+main proc
+mov ax,@data        ;TO ACCESS THE VARIABLE
+mov ds,ax
+mov ah,1 
+int 21h
+mov bl,2
+div bl
+cmp ah,0
+
+JE IsEven           ;IF NUMBER IS EVEN THIS LINE IS IGNORED
+mov dx,10           ;SPACE
+mov ah,2
+int 21h
+mov dx,13           ;CARRIAGE RETURN
+mov ah,2
+int 21h
+mov dx,offset Var_1
+mov ah,9
+int 21h
+mov ah,4ch          ;END THE PROGRAM
+int 21h
+IsEven:             ;IF NUMBER IS ODD THIS LINE IS IGNORED
+
+mov dx,10           ;SPACE
+mov ah,2
+int 21h
+mov dx,13           ;CARRIAGE RETURN
+mov ah,2
+int 21h
+mov dx,offset Var_2
+mov ah,9
+int 21h
+mov ah,4ch          ;END THE PROGRAM
+int 21h
+main endp
+end main
+
+
+
+
+
+
+
+
+
+
---
+++ file13.asm
@@ -0,0 +1,43 @@
+;Program to reverse a string.
+Dosseg
+.model small
+.stack 100h
+.data
+Msg db 'REVERSE$'       ;LENGTH=7
+.code
+main proc
+mov ax,@data
+mov ds,ax
+lea dx,Msg
+mov ah,9
+int 21h
+
+mov dx,10               ;SPACE
+mov ah,2
+int 21h
+mov dx,13               ;CARRAIGE RETURN
+mov ah,2
+int 21h
+
+lea si,Msg              ;SAVING STARTING INDEX OF MSG
+mov cx,7                 
+R1:
+mov dx,[si]
+push dx                 ;PUSH VALUE FROM DX TO STACK
+inc si
+Loop R1
+
+mov cx,7
+R2:
+pop dx                  ;POP VALUE FROM DX TO STACK
+mov ah,2
+int 21h
+Loop R2
+mov ah,4ch              ;END THE PROGRAM
+int 21h
+main endp
+end main
+
+
+
+
